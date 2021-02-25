0ba2614b436deb0b8d95ee3ed8751c83e47d569e
HyungjuJang <dacapolife87@gmail.com>
2021-02-26 00:13:30 +0900

    feat : DDD Start 책 프로젝트 생성

---
+++ src/test/java/me/hjjang/dddstart/DddstartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjjang.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
