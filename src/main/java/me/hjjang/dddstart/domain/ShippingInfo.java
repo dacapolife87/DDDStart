903d967a03e6eefe979edd38f7dbba7489b5d29d
HyungjuJang <dacapolife87@gmail.com>
2021-02-26 07:47:08 +0900

    feat : 배송 / 수신자 / 집주소 관련 도메인

---
+++ src/main/java/me/hjjang/dddstart/domain/ShippingInfo.java
@@ -0,0 +1,20 @@
+package me.hjjang.dddstart.domain;
+
+public class ShippingInfo {
+
+    private Receiver receiver;
+    private Address address;
+
+    public ShippingInfo(Receiver receiver, Address address) {
+        this.receiver = receiver;
+        this.address = address;
+    }
+
+    public Receiver getReceiver() {
+        return receiver;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
