d26512f367d002c740928f05c737816e1f5f1c71
HyungjuJang <dacapolife87@gmail.com>
2021-02-26 21:45:36 +0900

    feat : 주문번호 금액 일급컬렉션

---
+++ src/main/java/me/hjjang/dddstart/domain/OrderNo.java
@@ -0,0 +1,17 @@
+package me.hjjang.dddstart.domain;
+
+public class OrderNo {
+
+    private String number;
+
+    public OrderNo() {
+    }
+
+    public OrderNo(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
